41def8c4fba5f32ca98c90f705556b3f97897cbf
Chrissmbinoy <71966593+Chrissmbinoy@users.noreply.github.com>
2020-10-23 15:43:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2,box3,ground;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    box3 = new Box(220,200,50,100);
+    box2 = new Box(240,100,50,100);
+    ground = new Ground(200,height,400,20)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box3.body.position.x);
+    console.log(box3.body.position.y);
+    console.log(box3.body.angle);
+    
+    box1.display();
+    box2.display();
+    box3.display();
+    ground.display();
+}
